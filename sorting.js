94ceef4a575bf73593de95db1cc698f2f3fe93fb
Polina Grabar <polina.grabar@gmail.com>
2021-07-27 18:02:12 +0400

    finished 3/6 sorting algos

--- sorting.js
+++ sorting.js
@@ -1,7 +1,10 @@
 // ------- SORTING ALGORITHMS -------
 
-function swap(item1, item2) {
+function swap(arr, index1, index2) {
     return new Promise(resolve => {
+        let item1 = arr[index1];
+        let item2 = arr[index2];
+
         let tf1 = getComputedStyle(item1).getPropertyValue("transform");
         let tf2 = getComputedStyle(item2).getPropertyValue("transform");
 
@@ -10,92 +13,52 @@ function swap(item1, item2) {
 
         requestAnimationFrame(
             function() {
-                setTimeout(
-                    () => {
-                        // field.insertBefore(item2, item1);
-                        [item1, item2] = [item2, item1];
-                        resolve();
-                    }, delayMilliseconds);
+                setTimeout(() => {
+                    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+                    resolve();
+                }, delayMilliseconds);
             }); 
     }); 
 }
 
-// function arrswap(arr, index1, index2) {
-//     return new Promise(resolve => {
-//         let item1 = arr[index1];
-//         let item2 = arr[index2];
-//         let tf1 = getComputedStyle(item1).getPropertyValue("transform");
-//         let tf2 = getComputedStyle(item2).getPropertyValue("transform");
-
-//         item1.style.transform = tf2;
-//         item2.style.transform = tf1;
-
-//         requestAnimationFrame(
-//             function() {
-//                 setTimeout(
-//                     () => {
-//                         [arr[item1], arr[item2]] = [arr[item2], arr[item1]];
-//                         resolve();
-//                     }, delayMilliseconds);
-//             }); 
-//     }); 
-// }
-
-async function bubbleSort() {
-    let itemArray = document.querySelectorAll(".sortItem");
-
+async function bubbleSort(itemArray) {
     for (let i = 0; i < itemArray.length; i++) {
         for (let j = 0; j < itemArray.length - i - 1; j++) {
             let item1 = itemArray[j];
             let item2 = itemArray[j + 1]; 
 
-            item1.style.backgroundColor = selectedItemColour;
-            item2.style.backgroundColor = selectedItemColour;
-
+            setItemPairColour(item1, item2, selectedItemColour);
             await new Promise(resolve => setTimeout(() => { resolve(); }, delayMilliseconds));
 
             if (getValue(item1) > getValue(item2)) {
-                await swap(item1, item2);
-                itemArray = document.querySelectorAll(".sortItem");
+                await swap(itemArray, j, j + 1);
             }
-
-            item1.style.backgroundColor = defaultItemColour;
-            item2.style.backgroundColor = defaultItemColour;
+            setItemPairColour(item1, item2);
         }
     }
 }
 
-async function insertionSort() {
-    let itemArray = Array.from(document.querySelectorAll(".sortItem"));
-
+async function insertionSort(itemArray) {
     for (let i = 1; i < itemArray.length; i++) {
         for (let j = i; j > 0; j--) {
             let item1 = itemArray[j];
             let item2 = itemArray[j - 1];
 
-            item1.style.backgroundColor = selectedItemColour;
-            item2.style.backgroundColor = selectedItemColour;
-
+            setItemPairColour(item1, item2, selectedItemColour);
             await new Promise(resolve => setTimeout(() => { resolve(); }, delayMilliseconds));
 
             if (getValue(item1) >= getValue(item2)) {
-                item1.style.backgroundColor = defaultItemColour;
-                item2.style.backgroundColor = defaultItemColour;
+                setItemPairColour(item1, item2);
                 break;
             }
-            // await arrswap(itemArray, j, j - 1);
-            await swap(item1, item2);
-            [itemArray[j], itemArray[j - 1]] = [itemArray[j - 1], itemArray[j]] 
-
-            item1.style.backgroundColor = defaultItemColour;
-            item2.style.backgroundColor = defaultItemColour;
+            await swap(itemArray, j, j - 1);
+            setItemPairColour(item1, item2);
         }
     }
 }
 
 // ye this doesn't work 
-async function shellSort() {
-    let itemArray = document.querySelectorAll(".sortItem");
+async function shellSort(itemArray) {
     for (let gap = ~~(itemArray.length / 2); gap > 0; gap = ~~(gap / 2)) {
         for (let i = gap, j; i < itemArray.length; i++) {
 
@@ -106,38 +69,55 @@ async function shellSort() {
                 let item1 = itemArray[j];
                 let item2 = itemArray[j - gap];
                 
-                item1.style.backgroundColor = selectedItemColour;
-                item2.style.backgroundColor = selectedItemColour;
-
+                setItemPairColour(item1, item2, selectedItemColour);
                 await new Promise(resolve => setTimeout(() => { resolve(); }, delayMilliseconds));
                 
                 if (getValue(item2) > getValue(temp)) {
                     // itemArray[j] = itemArray[j - gap];
                     await swap(item1, item2);
-                    itemArray = document.querySelectorAll(".sortItem");
                 }
 
-                item1.style.backgroundColor = defaultItemColour;
-                item2.style.backgroundColor = defaultItemColour;
-
+                setItemPairColour(item1, item2);
             }
-            await swap(temp, itemArray[j])
+            await swap(temp, itemArray[j]);
             temp.style.backgroundColor = defaultItemColour;
             // itemArray[j] = temp;
         }
     }
 }
 
-async function selectionSort() {
-    let itemArray = document.querySelectorAll(".sortItem");
+async function selectionSort(itemArray) {
+    for (let i = 0; i < itemArray.length - 1; i++) {
+        let minValueIndex = i;
+        let minItem, nextItem;
+
+        for (let j = i + 1; j < itemArray.length; j++) {
+            minItem = itemArray[minValueIndex];
+            nextItem = itemArray[j];
+
+            setItemPairColour(minItem, nextItem, selectedItemColour);
+            await new Promise(resolve => setTimeout(() => { resolve(); }, delayMilliseconds));
+
+            if (getValue(nextItem) < getValue(minItem)) {
+                minValueIndex = j;
+            }
+            setItemPairColour(minItem, nextItem);
+        }
+        
+        let currentItem = itemArray[i];
+        setItemPairColour(minItem, currentItem, selectedItemColour);
+        await new Promise(resolve => setTimeout(() => { resolve(); }, delayMilliseconds));
+
+        await swap(itemArray, minValueIndex, i);
+        setItemPairColour(minItem, currentItem);
+        await new Promise(resolve => setTimeout(() => { resolve(); }, delayMilliseconds));
+    }
 }
 
-async function heapSort() {
-    let itemArray = document.querySelectorAll(".sortItem");
+async function heapSort(itemArray) {
 }
 
-async function quickSort() {
-    let itemArray = document.querySelectorAll(".sortItem");
+async function quickSort(itemArray) {
 }
 // -------------
 
@@ -147,6 +127,11 @@ async function quickSort() {
 
 
 // ------- WINDOW UTILITY FUNCTIONS -------
+
+function setItemPairColour(item1, item2, colour = defaultItemColour) {
+    item1.style.backgroundColor = colour;
+    item2.style.backgroundColor = colour;
+}
 
 function resetWindowState() {
     removeChildren(field);
@@ -176,8 +161,8 @@ function generateItemArray(arraySize) {
 function getItemHeightModifier() {
 
     // let fh = field.clientHeight;
-    // let item1 = fh / maxItemValue;
-    // let res = Math.floor(item1) - 1;
+    // let temp = fh / maxItemValue;
+    // let res = Math.floor(temp) - 1;
 
     // return Math.floor(field.height / maxItemValue) - 1;
 
@@ -236,7 +221,9 @@ function disableSortButtons(disable) {
 function runSorting(sortFunction, sortName) {
     disableSortButtons(true);
     updateTimerMessage(sortName);
-    sortFunction();
+
+    let itemArray = Array.from(document.querySelectorAll(".sortItem"));
+    sortFunction(itemArray);
 }
 // -------------
 
@@ -285,15 +272,3 @@ window.onload = function() {
         runSorting(quickSort, "Quick Sort");
     };
 }
-
-function insSort() {
-    let arr = [5, 4, 1, 6, 10, 8, 3, 9, 7, 2];
-    for (let i = 1; i<arr.length; i++) {
-        for (let j = i; j > 0 && arr[j] < arr[j-1]; j -= 1) {
-            [arr[j], arr[j-1]] = [arr[j-1], arr[j]]; 
-        }
-    }
-    console.log(arr);
-}
-
-insSort();
